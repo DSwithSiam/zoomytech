2373820944e64eb0b330760ec47fcbd6f012ef6d
Md Masipul Islam Siam <masipulislam@gmail.com>
2025-05-06 16:28:55 +0600

    update

--- zoomytech/settings.py
+++ zoomytech/settings.py
@@ -137,7 +137,7 @@ CORS_ALLOWED_ORIGINS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+ 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
