474344d2f4140fa02fd63f59e4ed8ad4875c20a1
DSwithSiam <masipulislam@gmail.com>
2025-05-08 19:45:41 +0600

    update

--- contract/urls.py
+++ contract/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path('draft-proposals/delete/', delete_draft_proposal, name='delete_draft_proposal'),
     path('draft-proposals/list/', draf_proposal_list, name='draft-proposals'),
     path('submitted-proposals/', submit_proposal_list, name='submitted-proposals'),
-    path('proposal/', get_and_update_proposal_by_id, name='proposal'),
+    path('proposal/<int:proposal_id>/', get_and_update_proposal_by_id, name='proposal'), #get and update proposal by id
     # path('download-pdf/', download_pdf, name='download-pdf'),
     # path('send-pdf-email/', send_pdf_email, name='send-pdf-email'),
-    path('proposal_pdf/', proposal_pdf, name='proposal_pdf'),
+    path('proposal_pdf/<int:proposal_id>/', proposal_pdf, name='proposal_pdf'),
 ]
 
